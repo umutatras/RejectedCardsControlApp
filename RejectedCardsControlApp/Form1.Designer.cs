fb6483ae4e9e4f1a690ed25dba6360deed62709e
Umut Atraş <umutatras@outlook.com>
2024-10-17 10:08:40 +0300

    .

--- RejectedCardsControlApp/Form1.Designer.cs
+++ RejectedCardsControlApp/Form1.Designer.cs
@@ -29,55 +29,98 @@ namespace RejectedCardsControlApp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(169, 137);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.button1.Location = new System.Drawing.Point(78, 223);
             this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(225, 70);
+            this.button1.Size = new System.Drawing.Size(239, 51);
             this.button1.TabIndex = 0;
             this.button1.Text = "Kartı Test Et";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // comboBox1
             // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(169, 44);
+            this.comboBox1.Location = new System.Drawing.Point(78, 182);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(225, 21);
+            this.comboBox1.Size = new System.Drawing.Size(239, 23);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(11, 44);
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(115, 145);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.Size = new System.Drawing.Size(171, 21);
             this.label1.TabIndex = 2;
             this.label1.Text = "Kart Okuyucu Seçiniz";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(78, 290);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(239, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Kart Okuyucu Listesini Getir";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(399, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 267);
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(403, 399);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximumSize = new System.Drawing.Size(419, 438);
+            this.MinimumSize = new System.Drawing.Size(419, 438);
             this.Name = "Form1";
-            this.Text = "Kart Kontrol Formu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kart Kontrol";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,6 +131,8 @@ namespace RejectedCardsControlApp
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- RejectedCardsControlApp/Form1.cs
+++ RejectedCardsControlApp/Form1.cs
@@ -15,7 +15,6 @@ namespace RejectedCardsControlApp
         {
 
             InitializeComponent();
-            ListSmartCardReaders(comboBox1);
 
         }
         string okuyucuName = "";
@@ -37,15 +36,30 @@ namespace RejectedCardsControlApp
 
                     portComboBox.Items.Clear();
                     portComboBox.Items.AddRange(readers.ToArray());
+                    if (readers.Length == 0)
+                    {
+                        MessageBox.Show(
+                            "Hata: Kart okuyucu listesini alırken bir sorun oluştu. Lütfen takılı bir okuyucu var mı kontrol ediniz.",
+                            "Hata",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error
+                        );
+                    }
                     if (readers.Length > 0)
                     {
                         portComboBox.SelectedIndex = 0; // Optional: Select the first reader by default
                     }
+
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Hata: " + "Kart okuyucu listesini alırken bir sorun oluştu.");
+                MessageBox.Show(
+                            "Hata: " + "Kart okuyucu listesini alırken bir sorun oluştu.",
+                            "Hata",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error
+                        );
             }
         }
 
@@ -64,26 +78,46 @@ namespace RejectedCardsControlApp
                         var atr = reader.GetAttrib(SCardAttribute.AtrString, out byte[] atrout);
                         var atrHex = BitConverter.ToString(atrout).Replace("-", "");
 
-                        if(string.IsNullOrEmpty(atrHex))
+                        if (string.IsNullOrEmpty(atrHex))
                         {
-                            MessageBox.Show("ATR bilgisi alınamadı.");
+                            MessageBox.Show(
+                           "Hata: " + "ATR bilgisi alınamadı.",
+                           "Hata",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Error
+                                );
                             return;
                         }
-                        MessageBox.Show("ATR Bilgisi: " + atrHex);
+                        MessageBox.Show(
+                      "ATR Bilgisi: " + atrHex,
+                      "ATR Bilgisi",
+                      MessageBoxButtons.OK,
+                      MessageBoxIcon.Information
+                           );
                     }
-                 
+
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Hata: " + "Lütfen kartınızı veya kart okuyucunuzu kontrol ediniz.");
+                MessageBox.Show(
+                         "Hata: " + "Lütfen kartınızın çipini veya kart okuyucunuzu kontrol ediniz.",
+                         "Hata",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error
+                              );
             }
         }
         public void SendApduCommand(string readerName)
         {
-            if(String.IsNullOrEmpty(okuyucuName))
+            if (String.IsNullOrEmpty(okuyucuName))
             {
-                MessageBox.Show("Lütfen kart okuyucu seçiniz.");                
+                MessageBox.Show(
+                         "Hata: " + "Lütfen kart okuyucu seçiniz.",
+                         "Hata",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error
+                              );
             }
             try
             {
@@ -96,7 +130,12 @@ namespace RejectedCardsControlApp
                         var rc = reader.Connect(readerName, SCardShareMode.Shared, SCardProtocol.Any);
                         if (rc != SCardError.Success)
                         {
-                            MessageBox.Show("Akıllı karta bağlanılamadı: " + SCardHelper.StringifyError(rc));
+                            MessageBox.Show(
+                         "Hata: " + "Akıllı karta bağlanılamadı: ",
+                         "Hata",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error
+                              );
                             return;
                         }
 
@@ -118,7 +157,12 @@ namespace RejectedCardsControlApp
 
                         if (rc != SCardError.Success)
                         {
-                            MessageBox.Show("APDU komutu aktarılamadı: " + SCardHelper.StringifyError(rc));
+                            MessageBox.Show(
+                         "Hata: " + "APDU komutu aktarılamadı.",
+                         "Hata",
+                         MessageBoxButtons.OK,
+                         MessageBoxIcon.Error
+                              );
                             return;
                         }
 
@@ -137,30 +181,126 @@ namespace RejectedCardsControlApp
 
                             if (rc != SCardError.Success)
                             {
-                                MessageBox.Show("APDU komutu aktarılamadı: " + SCardHelper.StringifyError(rc));
+                                MessageBox.Show("Hata: " + "APDU komutu aktarılamadı.",
+                                                       "Hata",
+                                                       MessageBoxButtons.OK,
+                                                       MessageBoxIcon.Error
+                                                            );
                                 return;
                             }
                         }
 
                         // Process the response
                         string responseHex = BitConverter.ToString(response).Replace("-", "");
-                        MessageBox.Show("APDU Yanıtı: " + responseHex);
 
-                     
-
-                            if (responseHex == "9000")
-                            {
-                                MessageBox.Show("Kart dolu ve işlem başarılı.");
-                            }
-                            else if (responseHex == "6A82")
-                            {
-                                MessageBox.Show("Kart boş veya istenen veri mevcut değil.");
-                            }
-                            else
-                            {
-                                MessageBox.Show("Bilinmeyen yanıt kodu: " + responseHex);
-                            }
-                      
+                        switch (responseHex)
+                        {
+                            case "9000":
+                                MessageBox.Show("Başarılı: " + "Kart dolu ve işlem başarılı." + responseHex,
+                                                "Başarılı",
+                                                MessageBoxButtons.OK,
+                                                MessageBoxIcon.Information
+                                                     );
+                                break;
+                            case "6A82":
+                                MessageBox.Show("Hata: " + "Kart boş veya istenen veri mevcut değil." + responseHex,
+                                                "Hata",
+                                                MessageBoxButtons.OK,
+                                                MessageBoxIcon.Error
+                                                     );
+                                break;
+                            case "6700":
+                                MessageBox.Show("Hata: " + "Yanlış uzunluk;daha fazla gösterge yok." + responseHex,
+                                                       "Hata",
+                                                       MessageBoxButtons.OK,
+                                                       MessageBoxIcon.Error
+                                                            );
+                                break;
+                            case "6982":
+                                MessageBox.Show("Hata: " + "Güvenlik durumu karşılanmadı." + responseHex,
+                                                      "Hata",
+                                                      MessageBoxButtons.OK,
+                                                      MessageBoxIcon.Error
+                                                           );
+                                break;
+                            case "6A86":
+                                MessageBox.Show("Hata: " + "Yanlış p1/p2 parametreleri." + responseHex,
+                                                    "Hata",
+                                                    MessageBoxButtons.OK,
+                                                    MessageBoxIcon.Error
+                                                         );
+                                break;
+                            case "6883":
+                                MessageBox.Show("Hata: " + "Zincirdeki son komut bekleniyor." + responseHex,
+                                               "Hata",
+                                               MessageBoxButtons.OK,
+                                               MessageBoxIcon.Error
+                                                    );
+                                break;
+                            case "6884":
+                                MessageBox.Show("Hata: " + "Bu apdu için komut zinciri desteklenmiyor." + responseHex,
+                                               "Hata",
+                                               MessageBoxButtons.OK,
+                                               MessageBoxIcon.Error
+                                                    );
+                                break;
+                            case "6A80":
+                                MessageBox.Show("Hata: " + "Yanlış veri." + responseHex,
+                                               "Hata",
+                                               MessageBoxButtons.OK,
+                                               MessageBoxIcon.Error
+                                                    );
+                                break;
+                            case "6D00":
+                                MessageBox.Show("Hata: " + "Talimat kodu desteklenmiyor veya geçersiz." + responseHex,
+                                               "Hata",
+                                               MessageBoxButtons.OK,
+                                               MessageBoxIcon.Error
+                                                    );
+                                break;
+                            case "6985":
+                                MessageBox.Show("Hata: " + "Kullanım koşulları karşılanmadı (geçerli anahtar yok, özel anahtar zaten mevcut)." + responseHex,
+                                               "Hata",
+                                               MessageBoxButtons.OK,
+                                               MessageBoxIcon.Error
+                                                    );
+                                break;
+                            case "6A84":
+                                MessageBox.Show("Hata: " + "Yeterli bellek alanı yok." + responseHex,
+                                               "Hata",
+                                               MessageBoxButtons.OK,
+                                               MessageBoxIcon.Error
+                                                    );
+                                break;
+                            case "6A89":
+                                MessageBox.Show("Hata: " + "Dosya hazır mevcut." + responseHex,
+                                              "Hata",
+                                              MessageBoxButtons.OK,
+                                              MessageBoxIcon.Error
+                                                   );
+                                break;
+                            case "6986":
+                                MessageBox.Show("Hata: " + "Komuta izin verilmiyor." + responseHex,
+                                              "Hata",
+                                              MessageBoxButtons.OK,
+                                              MessageBoxIcon.Error
+                                                   );
+                                break;
+                            case "6B00":
+                                MessageBox.Show("Hata: " + "Yanlış parametreler P1-P2." + responseHex,
+                                              "Hata",
+                                              MessageBoxButtons.OK,
+                                              MessageBoxIcon.Error
+                                                   );
+                                break;
+                            default:
+                                MessageBox.Show("Hata: " + "Bilinmeyen yanıt kodu: " + responseHex,
+                                              "Hata",
+                                              MessageBoxButtons.OK,
+                                              MessageBoxIcon.Error
+                                                   );
+                                break;
+                        }
 
                         reader.Disconnect(SCardReaderDisposition.Unpower);
                     }
@@ -168,13 +308,17 @@ namespace RejectedCardsControlApp
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Hata: " + ex.Message);
             }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
             okuyucuName = comboBox1.SelectedItem.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ListSmartCardReaders(comboBox1);
         }
     }
 }
